6074fc7db07248dbb43286d5c9a0c6e9a7645367
adrigar94 <112860345+adrigar94@users.noreply.github.com>
2023-05-30 08:13:23 +0000

    fix: override method create in userFullNameVO for create correct VO

--- src/User/Domain/UserFullname.php
+++ src/User/Domain/UserFullname.php
@@ -5,7 +5,12 @@ declare(strict_types=1);
 namespace ScooterVolt\UserService\User\Domain;
 
 use Adrigar94\ValueObjectCraft\Domain\Name\FullnameValueObject;
+use Adrigar94\ValueObjectCraft\Domain\Name\NameValueObject;
 
 class UserFullname extends FullnameValueObject
 {
+    public static function create(string $name, string $surname): self
+    {
+        return new Self(new NameValueObject($name), new NameValueObject($surname));
+    }
 }
