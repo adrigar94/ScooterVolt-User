e82680f7ee084f9f56ba8281b11658024674dc50
Adrián García <akrian231@gmail.com>
2023-07-11 09:08:24 +0200

    bugfix: override UserPassword@encode with NativePasswordHasher

--- src/User/Domain/UserPassword.php
+++ src/User/Domain/UserPassword.php
@@ -5,7 +5,14 @@ declare(strict_types=1);
 namespace ScooterVolt\UserService\User\Domain;
 
 use Adrigar94\ValueObjectCraft\Domain\Password\PasswordValueObject;
+use Symfony\Component\PasswordHasher\Hasher\NativePasswordHasher;
 
 class UserPassword extends PasswordValueObject
 {
+
+    public function encode(string $plainPassword): string
+    {
+        $hasher = new NativePasswordHasher();
+        return $hasher->hash($plainPassword);
+    }
 }
